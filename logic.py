be80500033bfe0fc622e99d7115ab4dbaf5c7233
shangxizhang <shangxizhang@hotmail.com>
2017-01-15 00:11:05 -0600

    logic

---
+++ logic.py
@@ -0,0 +1,87 @@
+import copy
+import math
+import random
+
+def new_board(row, col):
+    twod_list = []
+    for i in range(0,row):
+        oned_list = []
+        for j in range(0,col):
+            oned_list.append(0)
+        twod_list.append(oned_list)
+    rand_board = copy.deepcopy(twod_list)
+    rand_row = math.trunc(random.random() * row)
+    rand_col = math.trunc(random.random() * col)
+    rand_board[rand_row][rand_col] = 1
+    return rand_board
+
+def count(board,r,c):
+    row = len(board)
+    col = len(board[1])
+    up = (r - 1, c )
+    low = (r + 1, c )
+    left = (r , c - 1)
+    right = (r , c + 1)
+    up_left = (r - 1, c - 1)
+    up_right = (r - 1, c + 1)
+    low_left = (r + 1, c - 1)
+    low_right = (r + 1, c + 1)
+    elements = [up, low, left, right, up_left, up_right, low_left, low_right]
+    count = 0
+    if r == 0 and c == 0:
+        for element in [right, low_right, low]:
+            if board[element[0]][element[1]] != 0:
+                count += 1
+    elif r == 0 and c == col - 1:
+        for element in [left, low_left, low]:
+            if board[element[0]][element[1]] != 0:
+                count += 1
+    elif r == row - 1 and c == 0:
+        for element in [up, right, up_right]:
+            if board[element[0]][element[1]] != 0:
+                count += 1
+    elif r == row - 1 and c == col - 1:
+        for element in [left, up, up_left]:
+            if board[element[0]][element[1]] != 0:
+                count += 1
+    elif r == 0:
+        for element in [left, right, low, low_left, low_right]:
+            if board[element[0]][element[1]] != 0:
+                count += 1
+    elif r == row - 1:
+        for element in [left, right, up, up_left, up_right]:
+            if board[element[0]][element[1]] != 0:
+                count += 1
+    elif c == 0:
+        for element in [up, low, right, up_right, low_right]:
+            if board[element[0]][element[1]] != 0:
+                count += 1
+    elif c == col - 1:
+        for element in [up, low, left, up_left, low_left]:
+            if board[element[0]][element[1]] != 0:
+                count += 1
+    else:
+        for element in elements:
+            if board[element[0]][element[1]] != 0:
+                count += 1
+    return count
+
+def update_board(board):
+    num_of_rows = len(board)
+    num_of_cols = len(board[0])
+    original_board = copy.deepcopy(board)
+    for i in range(0,num_of_rows):
+        for j in range(0,num_of_cols):
+            board[i][j] = count(original_board,i,j)
+    counted_board = copy.deepcopy(board)
+    for i in range(0,num_of_rows):
+        for j in range(0,num_of_cols):
+            if counted_board[i][j] > 3 or counted_board[i][j] < 2 and original_board[i][j] != 0:
+                board[i][j] = 0 #if an alive cell has more than three or less than two adjacent alive cells, it dies
+            elif counted_board[i][j] == 3 and original_board[i][j] == 0:
+                board[i][j] = 1 #if a dead cell has three alive adjacent cells, it becomes alive
+            elif original_board[i][j] == 0:
+                board[i][j] = original_board[i][j]
+#            else:
+#                board[i][j] = original_board[i][j] + 1 #the value means age, every time self plus 1
+    return board
