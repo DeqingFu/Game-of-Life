8770f4ec7482cfcacd4aa037cf4c056182c4bc6f
Richard Zhang <richardzhang@wireless-s0-so-150-43-41.uchicago.edu>
2017-01-14 23:52:55 -0600

    single player, improved performance

---
+++ gui.py
@@ -0,0 +1,57 @@
+from logic import new_board, count, update_board
+from appJar import gui
+from Tkinter import *
+from Tkinter import Tk, Canvas, Frame, Button
+from Tkinter import BOTH, W, NW, SUNKEN, TOP, X, FLAT, LEFT
+
+canvas_width = 400
+canvas_height = 400
+colors = ("red", "blue")
+master = Tk()
+master.title("Game of Life")
+w = Canvas(master, width=canvas_width, height=canvas_height)
+
+# app=gui()
+
+
+test = new_board(20,20)
+
+def draw(L):
+	for i in range(len(L)):
+		for j in range (len(L[0])):
+			if (L[i][j] == 0):
+				w.create_rectangle(20*i, 20*j, 20*(i+1), 20*(j+1), fill = "red", outline = "black")
+			else:
+				w.create_rectangle(20*i, 20*j, 20*(i+1), 20*(j+1), fill = "blue", outline = "black")
+
+def flip(event):
+	x = event.x / 20
+	y = event.y / 20
+	if (test[x][y] == 0):
+		test[x][y] = 1
+	draw(test)
+
+def handlekey(event):
+	update_board(test)
+	draw(test)
+
+w.bind("<Button-2>", handlekey)
+w.bind("<Button-1>", flip)
+w.pack()
+
+message = Label( master, text = "Press the mouse to draw" )
+message.pack( side = BOTTOM )
+
+mainloop()
+
+# add a button
+# app.addButton("One", update_board)
+
+# app.go()
+
+# function called by pressing the buttons
+#def press(btn):
+#    if btn=="Cancel":
+#        app.stop()
+#    else:
+#        print("User:", app.getEntry('user'), "Pass:", app.getEntry('pass'))
