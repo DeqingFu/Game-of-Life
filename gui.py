9fe08a4b51218cd42e0971151685358c7f852d7d
shangxizhang <shangxizhang@hotmail.com>
2017-01-15 03:33:27 -0600

    the first working one

--- gui.py
+++ gui.py
@@ -4,56 +4,66 @@ from Tkinter import *
 from Tkinter import Tk, Canvas, Frame, Button
 from Tkinter import BOTH, W, NW, SUNKEN, TOP, X, FLAT, LEFT
 
-
 canvas_width = 600
 canvas_height = 600
 colors = ("red", "blue")
 master = Tk()
 master.title("Game of Life")
 w = Canvas(master, width=canvas_width, height=canvas_height)
-
-# app=gui()
+count = 0
 
 board = new_board(20,20)
-initial_player1()
+var = 5
 
 def draw(L):
-	r = len(L)
-	c = len(L[0])
-	for i in range(0,r):
-		for j in range (0,c):
+	for i in range(len(L)):
+		for j in range (len(L[0])):
 			if (L[i][j] == 0):
-				w.create_rectangle(20*i, 20*j, 20*(i+1), 20*(j+1), fill = "red", outline = "black")
-			else:
-				w.create_rectangle(20*i, 20*j, 20*(i+1), 20*(j+1), fill = "blue", outline = "black")
-	w.create_rectangle(40, 400, 160, 440, fill = "white", outline = "black")
-	w.create_text(100, 420, text = "update")
-	w.create_rectangle(240, 400, 360, 440, fill = "white", outline = "black")
-	w.create_text(300, 420, text = "reset")
+                                color = "white"
+                        elif (L[i][j] == 1):
+                                color = "red"
+                        else:
+                                color = "blue"
+			w.create_rectangle(20*i, 20*j, 20*(i+1), 20*(j+1), fill = color, outline = "black")
+
+w.create_rectangle(40, 400, 160, 440, fill = "white", outline = "black")
+w.create_text(100, 420, text = "update")
+w.create_rectangle(240, 400, 360, 440, fill = "white", outline = "black")
+w.create_text(300, 420, text = "reset")
+
 
 def reset():
-	return new_board(20,20)
+        global board
+        global count
+        board = new_board(20,20)
+        count = 0
+
 
 def click(event):
-	x = event.x / 20
-	y = event.y / 20
-	if y >= 20:
-		if x >= 2 and x <= 8:
-			update_board(board)
-		if x >= 12 and x <= 18:
-			reset()
-	elif board[x][y] == 0:
-		board[x][y] = 1
-	draw(board)
-	report()
+        global count
+        x = event.x / 20
+        y = event.y / 20
+        if y >= 20:
+                if x >= 2 and x <= 8:
+                        update_board(board)
+                if x >= 12 and x <= 18:
+                        reset()
+        elif board[x][y] == 0:
+                if count < var or (count % 2 == 0 and count >= var*2):
+                        board[x][y] = 1
+                else:
+                        board[x][y] = 2
+                count += 1
+        draw(board)
+        report()
+        print count
+
 
 def report():
 	a = 0
 	b = 0
-	r = len(board)
-	c = len(board[0])
-	for i in range(0,r):
-		for j in range(0,c):
+	for i in range(len(board)):
+		for j in range (len(board[0])):
 			if board[i][j] == 1:
 				a = a + 1
 			if board[i][j] == 2:
@@ -72,16 +82,3 @@ message.pack( side = BOTTOM )
 
 
 mainloop()
-
-
-# add a button
-# app.addButton("One", update_board)
-
-# app.go()
-
-# function called by pressing the buttons
-#def press(btn):
-#    if btn=="Cancel":
-#        app.stop()
-#    else:
-#        print("User:", app.getEntry('user'), "Pass:", app.getEntry('pass'))
